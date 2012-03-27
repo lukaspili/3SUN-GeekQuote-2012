199360bc9123b4c98712b49f13285ed96877c254
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2012-03-27 07:29:52 +0200

    Step 3 : Add quote adding text view to layout.

--- src/com/supinfo/geekquote/QuoteListActivity.java
+++ src/com/supinfo/geekquote/QuoteListActivity.java
@@ -2,20 +2,26 @@ package com.supinfo.geekquote;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.supinfo.geekquote.model.Quote;
 
-public class QuoteListActivity extends Activity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+public class QuoteListActivity extends Activity {
+	
+	private LinearLayout linearLayout;
+    
 	private ArrayList<Quote> quotes = new ArrayList<Quote>();
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        linearLayout = (LinearLayout) findViewById(R.id.vertical_layout);
         
         String[] quotesArray = getResources().getStringArray(R.array.quotes);
         
@@ -25,7 +31,18 @@ public class QuoteListActivity extends Activity {
     }
     
     public void addQuote(String strQuote) {
+    	
     	quotes.add(new Quote(strQuote));
+    	
+    	TextView textView = new TextView(this);
+    	textView.setText(strQuote);
+    	
+    	if(quotes.size() % 2 != 0) {
+    		textView.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+    	}
+    	 
+    	linearLayout.addView(textView);
+    	
     	Toast.makeText(this, "Quote ajouté", 5000).show();
     }
 }
