097b5cf7920be2c83d7e132a6947f2d8e77aca1c
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2012-03-27 10:31:28 +0200

    Step 4 : Add button listener.

--- src/com/supinfo/geekquote/QuoteListActivity.java
+++ src/com/supinfo/geekquote/QuoteListActivity.java
@@ -6,6 +6,9 @@ import com.supinfo.geekquote.model.Quote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +16,10 @@ import android.widget.Toast;
 public class QuoteListActivity extends Activity {
 	
 	private LinearLayout linearLayout;
+	
+	private Button button;
+	
+	private EditText editText;
     
 	private ArrayList<Quote> quotes = new ArrayList<Quote>();
 	
@@ -22,6 +29,24 @@ public class QuoteListActivity extends Activity {
         setContentView(R.layout.main);
         
         linearLayout = (LinearLayout) findViewById(R.id.vertical_layout);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				String strQuote = editText.getText().toString();
+				editText.setText("");
+				
+				if(strQuote == null || strQuote.trim().isEmpty()) {
+					return;
+				}
+				
+				addQuote(strQuote);
+			}
+		});
         
         String[] quotesArray = getResources().getStringArray(R.array.quotes);
         
